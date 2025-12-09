b8a8de107f1965c943366e11f52e9d51515f00d7
Anton <antontv2019@gmail.com>
2025-12-09 22:15:57 +0200

    Added Visuals: class for frontend

---
+++ class_Visual.py
@@ -0,0 +1,173 @@
+from helper_functions import RED, RESET
+
+import os
+
+
+class Visual:
+
+    def __init__(self):
+        self.window = None
+        self.window_width = None
+        self.window_height = None
+        self.block_width = None
+        self.block_height = None
+
+
+    def create_window(self, window_width: int, window_height: int, block_width: int, block_height: int) -> int:
+        self.window_width = window_width
+        self.window_height = window_height
+        self.block_width = block_width
+        self.block_height = block_height
+
+        self.window = []
+
+        counter = 0
+
+        # Створюємо сітку [x][y]
+        for x in range(window_width):
+            colum = []
+            for y in range(window_height):
+
+                new_block = ["0" * block_width for _ in range(block_height)]
+                colum.append(new_block)
+                counter += 1
+            self.window.append(colum)
+
+        return counter
+
+    def _check_write_to_coordinate(self, x_cord: int, y_cord: int, content) -> bool:
+
+        if self.window is None:
+            print(f"{RED}First create window{RESET}")
+            return False
+
+        if x_cord > self.window_width:
+            print(f"{RED}X-coordinate is too large. Max possible value is {self.window_width}{RESET}")
+            return False
+
+        if y_cord > self.window_height:
+            print(f"{RED}Y-coordinate is too large. Max possible value is {self.window_height}{RESET}")
+            return False
+
+        if type(content) == str:
+            if len(content) > (self.block_height * self.block_width):
+                print(f"{RED}Your content is too large. Max possible len is {self.block_height * self.block_width}{RESET}")
+                return False
+
+        if type(content) == list:
+            characters = 0
+            for line in content:
+                characters += len(line)
+
+            if characters > (self.block_height * self.block_width):
+                print(f"{RED}Your content is too large. Max possible len is {self.block_height * self.block_width}{RESET}")
+                return False
+
+        return True
+
+    def write_str_to_coordinate(self, x_cord: int, y_cord: int, content: str):
+        # перевірка
+
+        if not self._check_write_to_coordinate(x_cord, y_cord, content):
+            return None
+
+
+        # запис по кординатам
+        # block = self.window[x_cord][y_cord]
+        #
+        # print(block)
+
+
+        content_words = content.split(" ")
+        current_block_line = 0
+        for index in range(len(content_words)):
+
+
+            word = content_words[index - (self.block_height - 1)]
+            print((self.block_height - 1) - index)
+            print(word)
+
+            if index == (len(content_words) - 1):
+                sep = " " * (self.block_width - len(word))
+            else:
+                sep = " "
+
+
+            if len(word) < self.block_width:
+                self.window[x_cord][y_cord][index - (self.block_height - 1)] = word + sep
+
+            else:
+                self.window[x_cord][y_cord][index - (self.block_height - 1)] = word + sep
+
+
+
+            if len(self.window[x_cord][y_cord][index - (self.block_height - 1)]) > self.block_width:
+
+                if current_block_line < (self.block_height - 1):
+                    current_block_line += 1
+                else:
+                    break
+
+        print(content_words)
+
+        self.window[x_cord][y_cord][0] = content + " "*(self.block_width - len(content))
+
+        return True
+
+
+
+    def print_window(self, sep: str):
+        # self.clear_window()
+
+        for y in range(self.window_height):
+            # [(self.window_height - 1) - y]
+
+            for line in range(self.block_height):
+
+                for x in range(self.window_width):
+
+                    print(self.window[x][(self.window_height - 1) - y][(self.block_height - 1) - line], end=sep,)
+
+                print()
+            # [(self.block_height - 1) - line]
+
+        pass
+
+
+    def clear_window(self):
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+
+vis = Visual()
+vis.create_window(3, 3, 6, 3)
+print()
+vis.write_str_to_coordinate(0, 0,   "to in")
+print()
+vis.print_window(" ")
+
+
+
+#
+# # метод виводу розкладу на тиждень
+# # переключення між тижнями
+#
+# # from tqdm import tqdm
+# # import time
+# #
+# # # range(100) — це загальна кількість кроків
+# # for i in tqdm(range(100), desc="Обробка файлів", ascii=False):
+# #     time.sleep(0.1) # Імітація роботи
+#
+#
+#
+# import time
+#
+# print("Запуск таймера...")
+#
+# for i in range(10, 0, -1):
+#     # \r повертає на початок. end="" забороняє новий рядок.
+#     # Пробіли в кінці потрібні, щоб затерти залишки довшого тексту
+#     print(f"\rЗалишилось часу: {i} секунд   ", end="")
+#     time.sleep(1)
+#
+# print("\rГотово!                   ")
