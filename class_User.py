87661286951a32e03d57e6f0c47376b46506b1af
clafish <andrew.korzh2008@gmail.com>
2025-12-11 20:52:23 +0200

    finished(i hope so) with class_User's enroll method

--- class_User.py
+++ class_User.py
@@ -5,25 +5,70 @@ class User:
         self.name = name
         self.program = program
         self.disciplines = disciplines
+        self.counter = 0
 
     def __str__(self):
         return f"{self.name} is on {self.program} and enrolled to {self.disciplines}"
 
     def __checker__(self, discipline):
-        counter = 0
-        with (open("schedule.json", "r") as file):
-            for line in file.readlines():
-                schedule = json.loads(line)
+        with open("schedule.json", "r") as file:
+                schedule = json.load(file)
+
                 for day in schedule:
-                    for pair in day:
-                        for subject in pair.values():
-                            if subject != discipline or subject in self.disciplines:
-                                continue
-                            elif subject == discipline and not subject in self.disciplines:
-                                counter = 1
-                            else:
-                                continue
-        return counter
+                        day = schedule[day]
+
+                        for pair in day:
+                            pair = day[pair]
+
+                            for room in pair:
+
+                                for mine in self.disciplines:
+
+                                    if mine in pair.values():
+                                        continue
+
+                                    elif pair[room] == discipline and not pair[room] in self.disciplines:
+                                        self.counter += 1
+                                        continue
+
+                                    else:
+                                        continue
+        return self.counter
+
+    def __help_enroll__(self, counter, discipline):
+        if counter == 1:
+            print(f"No way you can visit {discipline}")
+        elif counter == 2:
+            with open("users.json", "r") as file:
+                data = json.load(file)
+            data[self.name]["courses"].append(discipline)
+            with open("users.json", "w") as file:
+                json.dump(data, file, indent=2)
+            print(f"You was successfully enrolled to {discipline}")
+            return True
+        else:
+            return False
+        return False
+
+    def __help_enroll_2__(self, discipline, counter):
+        discipline = list(discipline)
+        if discipline[-1] == "1":
+            discipline[-1] = "2"
+            dis = "".join(discipline)
+            counter2 = self.__checker__(dis)
+            print(counter2)
+            counter += counter2
+            if not self.__help_enroll__(counter, dis):
+                print(f"You was not enrolled on {dis}")
+
+        elif discipline[-1] == "2":
+            discipline[-1] = "1"
+            dis = "".join(discipline)
+            counter2 = self.__checker__(dis)
+            counter += counter2
+            if not self.__help_enroll__(counter, dis):
+                print(f"You was not enrolled on {dis}")
+
 
     def enroll(self, discipline):
         if discipline in self.disciplines:
@@ -31,26 +76,17 @@ class User:
             return False
         else:
             counter = self.__checker__(discipline)
-            if counter == 1:
+            result = self.__help_enroll__(counter, discipline)
+            if result:
+                return True
+            else:
                 answers = ["y", "n"]
-                ans = input("No free places for this group. Do you want to try pick another group? [y/n]")
+                ans = input("No free places for this group. Do you want to try pick another group? [y/n]: ")
                 while not ans.lower() in answers:
-                    ans = input("Invalid answer. Do you want to try pick another group? [y/n]")
+                    ans = input("Invalid answer. Do you want to try pick another group? [y/n]: ")
                 if ans == "y":
-                    discipline = list(discipline)
-                    if discipline[:-1] == "1":
-                        discipline[:-1] = "2"
-                        dis = "".join(discipline)
-                        counter2 = self.__checker__(dis)
-                        counter += counter2
-                        if counter == 1:
-                            print("No free places for you on those discipline")
-                        elif counter == 2:
-                            with open("users.json", "w") as file:
-                                data = json.load(file)
-                                data[self.name]["courses"].append(discipline)
-                                print(f"You was successfully enrolled to {discipline}")
-                        return True
+                    self.__help_enroll_2__(discipline, counter)
+                    return False
                 else:
                     print(f"You was not enrolled on {discipline}")
                     return False
@@ -66,3 +102,4 @@ class User:
                     dct["courses"].remove(discipline)
             discipline.enrolled.remove(self.name)
             print("Leaved successfully")
+
