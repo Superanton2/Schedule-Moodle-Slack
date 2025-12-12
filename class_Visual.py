72df8ce374d499a55f7dc53953b7128078a9ee71
Anton <antontv2019@gmail.com>
2025-12-12 19:32:29 +0200

    Center day names in input_week_lessons method
    Refactor input_week_lessons to center day names and write to coordinate.

--- class_Visual.py
+++ class_Visual.py
@@ -208,8 +208,15 @@ class Visual:
 
     def input_week_lessons(self, schedule_data: dict[str, dict] ):
         # {"Monday": {"Pair1": {"1.08.01": CS201"}}}
+
+        # запсужмо дні
         for day in ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]:
-            self.input_day_lessons(day, schedule_data[day])
+            x_encoded_day = day_encoder[day]
+
+            spaces = " " * ((self.block_width - len(day)) // 2)
+            day_centered = f"{spaces}{day}{spaces}"
+
+            self.write_lst_to_coordinate(x_encoded_day, 4, ["", f"{day_centered.upper():}", "", ""])
 
         for day in schedule_data:
             self.input_day_lessons(day, schedule_data[day])
